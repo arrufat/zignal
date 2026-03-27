30a322d58251d89f48a55996a493cdde655cab6b
Adrià Arrufat <adria.arrufat@gmail.com>
2026-03-27 12:00:09 +0900

    refactor(color): use extern struct for Rgba
    Changes the Rgba struct from packed to extern to ensure C-compatible
    memory layout.

--- src/color.zig
+++ src/color.zig
@@ -387,7 +387,7 @@ pub fn Rgba(comptime T: type) type {
         .int => if (T != u8) @compileError("Unsupported backing type " ++ @typeName(T) ++ " for color space"),
         else => @compileError("Unsupported backing type " ++ @typeName(T) ++ " for color space"),
     }
-    return packed struct {
+    return extern struct {
         pub const space: ColorSpace = .rgba;
         r: T,
         g: T,
